cb7a783a085b0da982e2d042f0bc587c8e409a23
Arik Ahsan <arikahsan156@gmail.com>
2024-03-04 18:27:19 -0500

    it works now; gs should work

--- 2024/crescendo_gs_config.js
+++ 2024/crescendo_gs_config.js
@@ -1,11 +1,13 @@
 var config_data = `
 {
   "dataFormat": "tsv",
+  "enable_google_sheets": "true",
   "title": "VDScout 2024",
   "page_title": "Crescendo by VDScout",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter's Initials",
+      "gsCol": "scouter",
       "code": "s",
       "type": "scouter",
       "size": 5,
